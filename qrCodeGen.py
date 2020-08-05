16c2daff7c7077f9ca812389c22db877f76fcf74
isudy <6219625+i-sudy@users.noreply.github.com>
2020-08-05 10:25:48 -0400

    qrcodegen
    Generate QR Code

---
+++ qrCodeGen.py
@@ -0,0 +1,9 @@
+# installation pip install pillow qrcode
+from PIL import Image , ImageDraw
+from datetime import datetime
+import qrcode
+val = input("Enter your value: ")
+data = val
+image = qrcode.make(data)
+image.save(val +"_"+datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ".png")
+
